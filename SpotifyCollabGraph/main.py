dfa022450276f7556970a21b6aeb7af1f3cc6ef8
Stoica Mihai <82443072+KOTerra@users.noreply.github.com>
2025-03-06 23:36:53 +0200

    loading cached data

--- SpotifyCollabGraph/main.py
+++ SpotifyCollabGraph/main.py
@@ -1,4 +1,5 @@
 import time
+import os
 import matplotlib.pyplot as plt
 import networkx as nx
 import spotipy
@@ -6,13 +7,15 @@ from spotipy.oauth2 import SpotifyClientCredentials
 import discogs_client
 import requests
 
-# tokens
+# API tokens
 client_credentials_manager = SpotifyClientCredentials(client_id='9a324ce2de004464916503c99da4c2f2',
                                                       client_secret='6855817946bf4204b187537c4c47dd70')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 discogs = discogs_client.Client('ExampleApplication/0.1', user_token='caNEgSDjmfRnyvXcZKRUfhLgSZcZnvgsYJdzCGLM')
 
 playlist_id = '1ssL2ME8jwjHRVKf7Cnbur'
+
+GRAPH_FILE = 'collaboration_graph_from_playlist.graphml'
 
 artist_bands = {}
 
@@ -40,19 +43,16 @@ def discogs_test():
         print(f"- {track.title}")
 
 
-# Function to check Discogs rate limit and handle rate-limiting errors
 def check_discogs_rate_limit():
-    # Perform a dummy API call to check the rate limits
     response = requests.get("https://api.discogs.com/", headers={'User-Agent': 'Your-Unique-App/1.0'})
 
-    # Check for rate limiting headers
     rate_limit_remaining = int(response.headers.get('X-Discogs-Ratelimit-Remaining', 1))
     rate_limit_reset = int(response.headers.get('X-Discogs-Ratelimit-Reset', time.time() + 60))
 
     if rate_limit_remaining == 0:
         reset_time = rate_limit_reset - time.time()
         print(f"Discogs rate limit reached. Sleeping for {reset_time + 5} seconds...")
-        time.sleep(reset_time + 5)  # Sleep until rate limit is reset
+        time.sleep(reset_time + 5)
 
 
 def get_band_members(band_name_str):
@@ -93,18 +93,22 @@ def get_playlist_tracks(playlist_id):
     return tracks
 
 
-def construct_graph_from_playlist(playlist_id):
-    G = nx.Graph()
+def load_graph_from_file(filename):
+    if os.path.exists(filename):
+        return nx.read_graphml(filename)
+    else:
+        return nx.Graph()
+
+
+def construct_graph_from_playlist(playlist_id, G):
     tracks = get_playlist_tracks(playlist_id)
 
-    #add all artists as nodes
     for track in tracks:
         artists = track['artists']
         for artist in artists:
             if artist not in G:
                 G.add_node(artist)
 
-                #get the bands of the artist from Discogs and add edges between the artist and their bands
                 band_memberships = get_band_members(artist)
                 if band_memberships:
                     for member in band_memberships:
@@ -117,8 +121,9 @@ def construct_graph_from_playlist(playlist_id):
         artists = track['artists']
         for i in range(len(artists)):
             for j in range(i + 1, len(artists)):
-                G.add_edge(artists[i], artists[j], song=track['name'])
-                print(f"{artists[i]} --with--> {artists[j]}")
+                if not G.has_edge(artists[i], artists[j]):
+                    G.add_edge(artists[i], artists[j], song=track['name'])
+                    print(f"{artists[i]} --with--> {artists[j]}")
 
     return G
 
@@ -138,8 +143,10 @@ def visualize_graph(G):
 if __name__ == '__main__':
     discogs_test()
 
-    G = construct_graph_from_playlist(playlist_id)
+    G = load_graph_from_file(GRAPH_FILE)
 
-    export_graph(G, 'collaboration_graph_from_playlist.graphml')
+    G = construct_graph_from_playlist(playlist_id, G)
+
+    export_graph(G, GRAPH_FILE)
 
     visualize_graph(G)
