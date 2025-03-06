5fa86084899c8aab69d7a38fac94f0ff7cb64feb
Stoica Mihai <82443072+KOTerra@users.noreply.github.com>
2025-03-06 17:49:19 +0200

    added discogs api and convenience methods

--- SpotifyCollabGraph/main.py
+++ SpotifyCollabGraph/main.py
@@ -1,18 +1,74 @@
+import time
+
+import matplotlib.pyplot as plt
 import networkx as nx
 import spotipy
-import matplotlib.pyplot as plt
 from spotipy.oauth2 import SpotifyClientCredentials
+import discogs_client
 
 # Authenticate with Spotify API
 client_credentials_manager = SpotifyClientCredentials(client_id='9a324ce2de004464916503c99da4c2f2',
                                                       client_secret='6855817946bf4204b187537c4c47dd70')
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+discogs = discogs_client.Client('ExampleApplication/0.1', user_token='caNEgSDjmfRnyvXcZKRUfhLgSZcZnvgsYJdzCGLM')
+
+playlist_id = '1ssL2ME8jwjHRVKf7Cnbur'
+
+artist_bands = {}
 
 
-# Function to retrieve tracks from a playlist
+def discogs_test():
+    artist_name = "Magna Carta Cartel"
+    results = discogs.search(artist_name, type="artist")
+
+    if results:
+        band = results[0]
+        print(f"Band: {band.name}, ID: {band.id}")
+    else:
+        print("No results found.")
+        exit()
+
+    artist = discogs.artist(band.id)
+    print("Members:", [member.name for member in artist.members])
+
+    release = artist.releases.page(0)[1]
+    year = f" ({release.year})" if hasattr(release, 'year') and release.year else ""
+    print(f"Release: {release.title} {year}")
+
+    print("Tracks:")
+    for track in release.tracklist:
+        print(f"- {track.title}")
+
+
+def get_band_members(band_name_str):
+    results = discogs.search(band_name_str, type="artist")
+
+    if results:
+        band = results[0]
+    else:
+        return None
+
+    artist = discogs.artist(band.id)
+    return artist.members
+
+
+def make_band_member_connection(artist_name):
+    band_memberships = artist_bands.get(artist_name)
+
+    for band in band_memberships:
+        add_band_to_artist(artist_name, band)
+
+
+def add_band_to_artist(artist_name, band_name):
+    if artist_name not in artist_bands:
+        artist_bands[artist_name] = []
+    if band_name not in artist_bands[artist_name]:
+        artist_bands[artist_name].append(band_name)
+
+
 def get_playlist_tracks(playlist_id):
     tracks = []
-    results = sp.playlist_tracks(playlist_id)
+    results = sp.playlist_items(playlist_id)
     for item in results['items']:
         track = item['track']
         artists = [artist['name'] for artist in track['artists']]
@@ -35,13 +91,13 @@ def construct_graph_from_playlist(playlist_id):
     # Add edges between artists
     for track in tracks:
         artists = track['artists']
+
         for i in range(len(artists)):
             for j in range(i + 1, len(artists)):
                 G.add_edge(artists[i], artists[j], song=track['name'])
     return G
 
 
-# Function to export graph in a format suitable for drawing
 def export_graph(G, filename):
     nx.write_graphml(G, filename)
 
@@ -56,7 +112,7 @@ def visualize_graph(G):
 
 
 if __name__ == '__main__':
-    playlist_id = '1ssL2ME8jwjHRVKf7Cnbur'
+    discogs_test()
 
     G = construct_graph_from_playlist(playlist_id)
 
